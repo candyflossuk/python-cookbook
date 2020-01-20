5a1a79073aecdbb61f0d1adaa79d5d6e6416e453
candyflossuk <ross.a.humphrey@googlemail.com>
2020-01-20 14:40:04 +0000

    Added example of unpacking N-element tuples and sequences

---
+++ data_structures_and_algorithms/unpacking_sequence_separate_variables.py
@@ -0,0 +1,30 @@
+"""
+N-element tuple or sequence that you want to unpack to a collection
+of N variables
+
+> Unpacking works with an object that is iterable (not just tuples and lists)
+> For values you are not interested in use a throwaway variable name '_'
+"""
+p = (4, 5)
+x, y = p
+print(x)  # 4
+print(y)  # 5
+
+data = ['ACME', 50, 91.1, (2012, 12, 21)]
+name, shares, price, date = data
+print(name)  # ACME
+print(shares)  # 50
+print(price)  # 91.1
+print(date)  # (2012, 12, 21)
+
+name, shares, price, (year, mon, day) = data
+print(name)  # ACME
+print(year)  # 2012
+print(mon)  # 12
+print(day)  # 21
+
+"""
+Mismatch in the number of elements will result in a ValueError 
+being thrown as there are more values to unpack than 
+values in the sequence
+"""
