a34c3a1f88c792ccade860da0d07a2ef7ce72ac3
candyflossuk <ross.a.humphrey@googlemail.com>
2020-04-02 09:49:46 +0100

    Added extra info to the printing with different separators and line endings script

--- files_and_io/printing_with_different_separator_or_line_ending.py
+++ files_and_io/printing_with_different_separator_or_line_ending.py
@@ -21,3 +21,19 @@ for i in range(5):
 for i in range(5):
     print(i, end=' ')
 # 0 1 2 3 4
+
+"""
+using print() with different item separator is often the easiest way to output data when you need something
+other than a space separating the items. Sometimes you will see programmers use str.join() ...
+"""
+print(','.join('ACME', '50', '91.5')) # DOES NOT WORK
+
+# To get this working you have to do
+row = ('ACME', 50, 91.5)
+print(','.join(row))
+# Even then it dosn't work so you have to do this trickery
+print(','.join(str(x) for x in row))
+
+# OR you could just do this
+print(*row, sep=',')
+
