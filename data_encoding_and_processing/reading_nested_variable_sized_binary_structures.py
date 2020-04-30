1012fdf7ff81356320a2ff7ef796702c300e7bb9
Ross Humphrey <4322944+candyflossuk@users.noreply.github.com>
2020-04-30 21:23:22 +0100

    Added first part of script that shows how write complex binary structures
    As per the title - this is the start of a larger script that shows how to write complex binary structures

---
+++ data_encoding_and_processing/reading_nested_variable_sized_binary_structures.py
@@ -0,0 +1,43 @@
+"""
+This script shows how to read complicated binary encoded data that
+contains nested and/or variable sized records. Such data includes
+images,videos etc
+
+The struct module can be used to decode and encode almost any kind
+of binary data structure. To illustrate the kind of data in question see below.
+"""
+import struct
+import itertools
+
+polys = [
+          [ (1.0, 2.5), (3.5, 4.0), (2.5, 1.5) ],
+          [ (7.0, 1.2), (5.1, 3.0), (0.5, 7.5), (0.8, 9.0) ],
+          [ (3.4, 6.3), (1.2, 0.5), (4.6, 9.2) ],
+        ]
+
+
+# To write this to a binary file of a defined structure use the following
+def write_polys(filename, polys):
+    # determine bounding box
+    flattened = list(itertools.chain(*polys))
+    min_x = min(x for x, y in flattened)
+    max_x = max(x for x, y in flattened)
+    min_y = min(y for x, y in flattened)
+    max_y = max(y for x, y in flattened)
+
+    with open(filename, 'wb') as f:
+        f.write(struct.pack('<iddddi',
+                            0x1234,
+                            min_x, min_y,
+                            max_x, max_y,
+                            len(polys)))
+
+    for poly in polys:
+        size = len(poly) * struct.calcsize('<dd')
+        f.write(struct.pack('<i', size+4))
+        for pt in poly:
+            f.write(struct.pack('<dd', *pt))
+
+            
+# Call it with polygon data
+write_polys('polys.bin', polys)
