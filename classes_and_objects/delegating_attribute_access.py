c21fc48937694ba57b617d41485d756b3a001820
candyflossuk <ross.a.humphrey@googlemail.com>
2020-06-18 19:17:47 +0100

    Added exaple of delegating to a proxy object

--- classes_and_objects/delegating_attribute_access.py
+++ classes_and_objects/delegating_attribute_access.py
@@ -44,4 +44,38 @@ class B:
         return getattr(self._a, item)
 
 
-#  Another example of delegation is the implementation of proxies that wrap around another object exposing its public attributes
+#  Another example of delegation is the implementation of proxies
+#  that wrap around another object exposing its public attributes
+class Proxy:
+    def __init__(self, obj):
+        self._obj = obj
+
+    # Delegate attribute lookup to internal obj
+    def __getattr__(self, item):
+        print("getattr:", item)
+        return getattr(self._obj, item)
+
+    # Delegate attribute assignment
+    def __setattr__(self, key, value):
+        if key.startswith("_"):
+            super().__setattr__(name, value)
+        else:
+            print("setattr:", name, value)
+            setattr(self._obj, name, value)
+
+    # Delegate attribute deletion
+    def __delattr__(self, item):
+        if item.startswith("_"):
+            super().__delattr__(item)
+        else:
+            print("delattr:", item)
+            delattr(self._obj, item)
+
+
+# Usage example
+class Spam:
+    def __init__(self, x):
+        self.x = x
+
+    def bar(self, y):
+        print("Spam.bar:", self.x, y)
