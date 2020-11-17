5a12a73e7cd4b211338f7aae31cb81d8ab7f475b
candyflossuk <ross.a.humphrey@googlemail.com>
2020-11-17 20:49:08 +0000

    Added an example of collecting gc manually, causing memory leaks in python as well as how to use weak refs and deref a weak ref

--- classes_and_objects/managing_memory_cyclic_data_structures.py
+++ classes_and_objects/managing_memory_cyclic_data_structures.py
@@ -44,3 +44,66 @@ root.add_child(cl)
 print(cl.parent)  # Node('parent')
 del root
 print(cl.parent)  # None  -  Rather than throwing
+
+# The usual rules of garbage collection often don't apply in Python
+
+# Class to illustrate when deletion occurs
+
+
+class Data:
+    def __del__(self):
+        print("Data.__del__")
+
+
+# Node class involving a cycle
+class Node:
+    def __init__(self):
+        self.data = Data()
+        self.parent = None
+        self.children = []
+
+    # DO NOT DO THIS - this will leak memory as the cycle is defining its own __del__ method
+    def __del__(self):
+        del self.data
+        del self.parent
+        del self.children
+
+    def add_child(self, child):
+        self.children.append(child)
+        child.parent = self
+
+
+a = Data()
+del a  # immediate delete
+
+a = Node()
+del a  # immediate delete
+
+a = Node()
+a.add_child(Node())
+del a  # Not deleted
+
+"""
+From the above you can see the objects are deleted apart from where a cycle is involved.
+A cycle in this case is where the parent references the child and vice versa.
+
+The reason that the object is not simply deleted is that Python's garbage collection
+is done using simple reference counting. When the reference count is 0 it is deleted.
+For cyclic structures this does not occur.
+
+To deal with cycles - there is a separate garbage collector that runs. A good rule here 
+is that you cnanot guarantee when it is run, and therefore you never know when the 
+cyclic data structures are collected. You can force GC 
+"""
+import gc
+
+gc.collect()  # Force collection
+
+# weak references eliminate ref cycles. This is a pointer that does not increase the ref count
+a = Node()
+a_ref = weakref.ref(a)
+
+# To deref a weak reference you call it like a function
+print(a_ref())
+
+# By using weak reference you will find you don't get reference cycles and that gc occurs immediately
