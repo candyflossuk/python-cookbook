163b7d297315c19c341540466d070fa34530b510
candyflossuk <ross.a.humphrey@googlemail.com>
2021-01-13 08:43:57 +0000

    Added a new modules and packages section - and added a script showing how hierarchical package of modules can be built out

---
+++ modules_and_packages/graphics/formats/__init__.py
@@ -0,0 +1,9 @@
+"""
+__init__.py files can be used to automatically load submodules as so...
+"""
+from . import jpg
+from . import png
+
+"""
+For this file a user has to only use a single import graphics.formats
+"""
---
+++ modules_and_packages/hierarchical_package_of_modules.py
@@ -0,0 +1,11 @@
+"""
+This script shows how to organize your code into a package
+consisting of a hierarchical collection of modules
+
+Under this folder 'modules_and_packages' a 'graphics' package
+structure has been created. This script shows how to import
+from a hierarchical package
+"""
+import graphics.primitive.line
+from graphics.primitive import line
+import graphics.formats.jpg as jpg
