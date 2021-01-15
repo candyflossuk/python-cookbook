b8066c6e395914ba087b2ec8befc910f21006c18
candyflossuk <ross.a.humphrey@googlemail.com>
2021-01-15 18:59:04 +0000

    Added an example of relative imports

---
+++ modules_and_packages/importing_package_submodules_using_relative_names.py
@@ -0,0 +1,8 @@
+"""
+This script shows how to import a submodule from another submodule within
+a package without hard coding the package name into the import statement.
+
+This is done using relative imports.
+
+See package 'mypackage' for examples
+"""
---
+++ modules_and_packages/mypackage/A/spam.py
@@ -0,0 +1,4 @@
+from . import grok
+from ..B import bar
+
+# This script shows how to carry out relative imports
