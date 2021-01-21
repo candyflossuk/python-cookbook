2f1d0c60d6a6881f9c2060c2d8897bc30d314d59
candyflossuk <ross.a.humphrey@googlemail.com>
2021-01-21 08:37:03 +0000

    Added a tiny set of instructions that demonstrate how to package up code in a zip and run it (where multiple files exist in a directory structure)

---
+++ modules_and_packages/making_directory_or_zip_runnable_as_main_script.py
@@ -0,0 +1,14 @@
+"""
+This script contains information on how to run a program consisting of multiple scripts by
+adding a __main__.py file and running the app (which will pick up main.py) and run it.
+
+add a __main__.py at the top level of the application
+
+run using:
+python3 myapplication
+Where myapplication is the name of the top level directory
+
+This also works if you package up your code into a zip as follows
+zip -r myapp.zip *.py
+python3 myapp.zip
+"""
