9e08f5d00d61de84dbd37e4a34c2764bae77a954
candyflossuk <ross.a.humphrey@googlemail.com>
2021-02-05 12:24:22 +0000

    Added a script that shows how to apply decorators to functions - to run a function when a module is loaded'

--- modules_and_packages/matching_modules_on_import.py
+++ modules_and_packages/matching_modules_on_import.py
@@ -20,3 +20,31 @@ class PostImportFinder:
             return None
         self._skip.add(fullname)
         return PostImportLoader(self)
+
+
+class PostImportLoader:
+    def __init__(self, finder):
+        self._finder = finder
+
+    def load_module(self, fullname):
+        importlib.import_module(fullname)
+        module = sys.modules[fullname]
+        for func in _post_import_hooks[fullname]:
+            func(module)
+        self._finder._skip.remove(fullname)
+        return module
+
+
+# Use this as the decorator using @when_imported('module_name')
+def when_imported(fullname):
+    def decorate(func):
+        if fullname in sys.modules:
+            func(sys.modules[fullname])
+        else:
+            _post_import_hooks[fullname].append(func)
+        return func
+
+    return decorate
+
+
+sys.meta_path.insert(0, PostImportFinder())
