6fcec7428a5eaf08592bbb9a138cdc3f0d3a1420
candyflossuk <ross.a.humphrey@googlemail.com>
2021-02-09 12:11:46 +0000

    Added example of installing packages for yourself (a simple one)

---
+++ modules_and_packages/installing_packages_just_for_yourself.py
@@ -0,0 +1,16 @@
+"""
+This script simply gives instructions on how
+to install third party packages.
+
+To install to the user python use directory use
+
+python3 setup.py install --user
+
+OR
+
+pip install --user packagename
+
+This means you can install packages without admin privileges.
+
+As an alternative - you can also create a virtual environment
+"""
