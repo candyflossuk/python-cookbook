1c6b751395216ff8a01d1ec8508cb77d3e35ffb4
candyflossuk <ross.a.humphrey@googlemail.com>
2021-02-10 09:24:28 +0000

    Added a small readme file that just outlines how a venv is constructed and the command to use

---
+++ modules_and_packages/creating_new_python_env.py
@@ -0,0 +1,12 @@
+"""
+This script shows how to create  new virtual environment using
+the pyenv command.
+
+pyenv Spam
+
+This will the create a directory - in which an environment can be created.
+An interpreter will be included to use within this directory also.
+
+This interpreter will have 'site-packages' - which is where third party
+packages will be installed.
+"""
