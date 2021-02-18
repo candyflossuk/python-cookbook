9394ae50650d71a1e03f77e80688a6b9f80926e0
candyflossuk <ross.a.humphrey@googlemail.com>
2021-02-18 08:19:12 +0000

    Added some examples of using the requests module as well as how to use the urlllib request to do similiar tasks.

--- network_web_programming/interacting_with_http_services_as_client.py
+++ network_web_programming/interacting_with_http_services_as_client.py
@@ -2,6 +2,7 @@
 This script shows how to access services via HTTP as a client
 """
 from urllib import request, parse
+import requests
 
 # Base url being accessed
 url = "http://httpbin.org/get"
@@ -15,3 +16,39 @@ querystring = parse.urlencode(parms)
 # Make a get request and read the response
 u = request.urlopen(url + "?" + querystring)
 resp = u.read()
+
+# And for a POST
+
+u = request.urlopen(url, querystring.encode("ascii"))
+
+# To supply custom headers - make a dictionary and create a Request instance
+
+headers = {"User-agent": "none/ofyourbusiness", "Spam": "Eggs"}
+
+req = request.Request(url, querystring.encode("ascii"), headers=headers)
+
+# Make a request and read the response
+u = request.urlopen(req)
+resp = u.read
+
+# If the request is more complex use the requests library - it has useful built in methods
+resp = requests.head("http://www.python.org/index.html")
+
+status = resp.status_code
+last_modified = resp.headers["last-modified"]
+content_type = resp.headers["content-type"]
+content_length = resp.headers["content-length"]
+
+# Using requests to login
+resp = requests.get(
+    "http://pypi.python.org/pypi?:action=login", auth=("user", "password")
+)
+
+# Taking cookies from one request to the next ...
+resp1 = requests.get(url)
+resp2 = requests.get(url, cookies=resp1.cookies)
+
+# And to upload content
+url = "http://httpbin.org/post"
+files = {"file": ("data.csv", open("data.csv", "rb"))}
+r = requests.post(url, files=files)
