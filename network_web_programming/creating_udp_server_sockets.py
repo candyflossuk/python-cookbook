43ea446e781b7aa38ce34de06f4f73843a0e16ae
candyflossuk <ross.a.humphrey@googlemail.com>
2021-03-16 20:21:31 +0000

    Added an example of a UDP server created using sockets (directly)

---
+++ network_web_programming/creating_udp_server_sockets.py
@@ -0,0 +1,19 @@
+"""
+This script shows how to create a UDP server directly using sockets
+"""
+from socket import socket, AF_INET, SOCK_DGRAM
+import time
+
+
+def time_server(address):
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.bind(address)
+    while True:
+        msg, addr = sock.recvfrom(8192)
+        print("Got message from", addr)
+        resp = time.ctime()
+        sock.sendto(resp.encode("ascii"), addr)
+
+
+if __name__ == "__main__":
+    time_server(("", 20000))
