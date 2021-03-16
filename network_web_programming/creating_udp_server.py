f29fd4ff437e4f68ac63b5be0fefb3f27ac87a57
candyflossuk <ross.a.humphrey@googlemail.com>
2021-03-16 20:18:57 +0000

    Added a ThreadingUDPServer

--- network_web_programming/creating_udp_server.py
+++ network_web_programming/creating_udp_server.py
@@ -3,7 +3,7 @@ This script shows how to communicate with clients over UDP
 
 To do so the socketserver library can be used
 """
-from socketserver import BaseRequestHandler, UDPServer
+from socketserver import BaseRequestHandler, UDPServer, ThreadingUDPServer
 import time
 
 
@@ -17,11 +17,17 @@ class TimeHandler(BaseRequestHandler):
         sock.sendto(resp.encode("ascii"), self.client_address)
 
 
+"""
 if __name__ == "__main__":
     serv = UDPServer(("", 20000), TimeHandler)
     serv.serve_forever()
 
-"""
+
 A typical UDP server receives an incoming datagram (message) + client address.
 If the server responds it sends a datagram back  - for this sendto() and
 recvfrom() should be used. """
+
+# Create a ThreadingUDPServer
+if __name__ == "__main__":
+    serv = ThreadingUDPServer(("", 20000), TimeHandler)
+    serv.serve_forever()
